1c8cdce574030793ab2dc3ba0354609913767d62
Pau García Chiner <chiner@MacBook-Air-de-Pau.local>
2023-03-08 15:46:10 +0100

    Remove unused telescope function.

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -3,10 +3,6 @@ if (not status) then return end
 local actions = require('telescope.actions')
 local builtin = require("telescope.builtin")
 
-local function telescope_buffer_dir()
-  return vim.fn.expand('%:p:h')
-end
-
 local fb_actions = require "telescope".extensions.file_browser.actions
 
 telescope.setup {
