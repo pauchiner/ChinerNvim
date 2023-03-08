c127ca4c690e9f81a2758aea0e1e3c90683854d7
Pau García Chiner <chiner@MacBook-Air-de-Pau.local>
2023-03-09 00:15:47 +0100

    Change the autotag plugin to a one more universal.

--- after/plugin/treesitter.rc.lua
+++ after/plugin/treesitter.rc.lua
@@ -22,9 +22,9 @@ ts.setup {
     "html",
     "lua"
   },
-  autotag = {
-    enable = true,
-  },
+  -- autotag = {
+  --  enable = true,
+  -- },
 }
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
