5e96054edc3ab3b024700db0fad09ea86c64bfb7
Pau García Chiner <chiner@MacBook-Air-de-Pau.local>
2023-03-09 00:16:58 +0100

    Set the background color of the floating windows.

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -4,3 +4,5 @@ vim.opt.winblend = 0
 vim.opt.wildoptions = 'pum'
 vim.opt.pumblend = 5
 vim.opt.background = 'dark'
+-- Set floating windows background
+vim.api.nvim_set_hl(0, 'NormalFloat', {bg='#2a2c3d'})
