bc08c5bc8972d101a8f606b48a4c1d65e8def479
Pau García Chiner <chiner@MacBook-Air-de-Pau.local>
2023-03-08 01:58:49 +0100

    Remove unused functions on lspconfig and its formatter.

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -5,35 +5,6 @@ if (not status) then return end
 
 local protocol = require('vim.lsp.protocol')
 
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
-  --Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-  -- Mappings.
-  local opts = { noremap = true, silent = true }
-
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  --buf_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  --buf_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
-
-  -- formatting
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = vim.api.nvim_create_augroup("Format", { clear = true }),
-      buffer = bufnr,
-      callback = function() vim.lsp.buf.format() end
-    })
-  end
-end
-
 protocol.CompletionItemKind = {
   '', -- Text
   '', -- Method
@@ -68,14 +39,12 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(
 )
 
 nvim_lsp.tsserver.setup {
-  on_attach = on_attach,
   filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
   cmd = { "typescript-language-server", "--stdio" },
   capabilities = capabilities
 }
 
 nvim_lsp.lua_ls.setup {
-  on_attach = on_attach,
   settings = {
     Lua = {
       diagnostics = {
