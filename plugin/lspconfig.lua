2da1a326eec18cf105a8d24d18d1c657cfe33c88
Pau García Chiner <chiner@MacBook-Air-de-Pau.local>
2023-03-09 00:17:51 +0100

    Update the tsserver to work with javascript

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -39,7 +39,7 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(
 )
 
 nvim_lsp.tsserver.setup {
-  filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+  filetypes = { "javascript", "javascriptreact", "javascript.jsx", "typescript", "typescriptreact", "typescript.tsx" },
   cmd = { "typescript-language-server", "--stdio" },
   capabilities = capabilities
 }
