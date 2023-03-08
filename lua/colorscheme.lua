a70f87fc7075fe4286381ecd3f8cf3e0d18586a5
Pau García Chiner <chiner@MacBook-Air-de-Pau.local>
2023-03-09 00:16:39 +0100

    Delete the notify message when the colorscheme doens't exists

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,7 +1,4 @@
 -- Put here the name of your colorscheme
 local colorscheme = "palenight"
 local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
+if not ok then return end
