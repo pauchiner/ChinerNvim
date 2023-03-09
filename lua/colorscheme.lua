85fa3d121d88f5b19c4e56930cabbbcdb73d9954
Pau García Chiner <chiner@MacBook-Air-de-Pau.local>
2023-03-09 11:33:26 +0100

    Remove project stuff and add the docusaurus template.

--- lua/colorscheme.lua
+++
@@ -1,4 +0,0 @@
--- Put here the name of your colorscheme
-local colorscheme = "palenight"
-local ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not ok then return end
--- lua/highlights.lua
+++
@@ -1,8 +0,0 @@
-vim.opt.cursorline = true
-vim.opt.termguicolors = true
-vim.opt.winblend = 0
-vim.opt.wildoptions = 'pum'
-vim.opt.pumblend = 5
-vim.opt.background = 'dark'
--- Set floating windows background
-vim.api.nvim_set_hl(0, 'NormalFloat', {bg='#2a2c3d'})
