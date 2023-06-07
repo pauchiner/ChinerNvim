21fceb36ee22c657b85e016f78a45033ffa8a9c9
Pau García Chiner <chiner@MacBook-Air-de-Pau.local>
2023-06-07 21:17:44 +0200

    Added autotag to the plugins

--- after/plugin/nvim-autopairs.rc.lua
+++ after/plugin/nvim-autopairs.rc.lua
@@ -1,4 +1,14 @@
-local status, autopairs = pcall(require, 'nvim-autopairs')
-if(not status) then return end
+local autopairs_status, autopairs = pcall(require, 'nvim-autopairs')
+local autotag_status, autotag = pcall(require, 'nvim-ts-autotag')
+
+if(not autopairs_status) then return end
 
 autopairs.setup{}
+
+if(not autotag_status) then return end
+
+autotag.setup {
+  autotag = {
+    enable = true
+  }
+}
