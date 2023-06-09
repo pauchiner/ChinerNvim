b3e8aa5a24c51e5d9fc92e551553283d0806153f
Pau García Chiner <chiner@MacBook-Air-de-Pau.local>
2023-06-09 22:39:40 +0200

    Update the diagnostic jump keybindings

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -17,7 +17,8 @@ saga.setup {
 }
 
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
 vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
