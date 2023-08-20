e629ec347fe3edc1668b05472df11fee408873e8
chiner <pauchiner@icloud.com>
2023-08-20 23:48:45 +0200

    Hide command line feedback

--- lua/base.lua
+++ lua/base.lua
@@ -34,6 +34,9 @@ vim.opt.wildignore:append { '*/node_modules/*' }
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
 
+-- Hide Insert and Visual line feedback
+vim.cmd(":set noshowmode");
+
 -- Turn off paste mode when leaving insert
 vim.api.nvim_create_autocmd("InsertLeave", {
   pattern = '*',
