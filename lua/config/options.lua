75626c8f4864d73600261cbc4645e6b3d6d286d3
chiner <pauchiner@icloud.com>
2023-09-13 10:33:15 +0200

    Update the options file

---
+++ lua/config/options.lua
@@ -0,0 +1,63 @@
+local options = {
+  autoindent     = true,                    --- Good auto indent
+  backspace      = "indent,eol,start",      --- Making sure backspace works
+  backup         = false,                   --- Recommended by coc
+  clipboard      = "unnamed,unnamedplus",   --- Copy-paste between vim and everything else
+  cmdheight      = 0,                       --- Give more space for displaying messages
+  completeopt    = "menu,menuone,noselect", --- Better autocompletion
+  concealcursor  = "",                      --- Set to an empty string to expand tailwind class when on cursorline 
+  cursorline     = true,                    --- Highlight of current line
+  emoji          = false,                   --- Fix emoji display
+  encoding       = "utf-8",                 --- The encoding displayed
+  errorbells     = false,                   --- Disables sound effect for errors
+  expandtab      = true,                    --- Use spaces instead of tabs
+  fileencoding   = "utf-8",                 --- The encoding written to file
+  foldcolumn     = "1",
+  foldnestmax    = 1,
+  foldlevel      = 99,                      --- Using ufo provider need a large value
+  foldlevelstart = 99,                      --- Expand all folds by default
+  ignorecase     = true,                    --- Needed for smartcase
+  incsearch      = true,                    --- Start searching before pressing enter
+  laststatus     = 2,                       --- Have a global statusline at the bottom instead of one for each window
+  mouse          = "a",                     --- Enable mouse
+  number         = true,                    --- Shows current line number
+  pumheight      = 10,                      --- Max num of items in completion menu
+  relativenumber = true,                    --- Enables relative number
+  scrolloff      = 8,                       --- Always keep space when scrolling to bottom/top edge
+  shiftwidth     = 2,                       --- Change a number of space characters inserted for indentation
+  showmode       = false,                   --- Don't show things like -- INSERT -- anymore
+  signcolumn     = "yes:1",                 --- Add extra sign column next to line number
+  smartcase      = true,                    --- Uses case in search
+  smartindent    = true,                    --- Makes indenting smart
+  smarttab       = true,                    --- Makes tabbing smarter will realize you have 2 vs 4
+  softtabstop    = 2,                       --- Insert 2 spaces for a tab
+  splitright     = true,                    --- Vertical splits will automatically be to the right
+  swapfile       = false,                   --- Swap not needed
+  tabstop        = 2,                       --- Insert 2 spaces for a tab
+  termguicolors  = true,                    --- Correct terminal colors
+  timeoutlen     = 200,                     --- Faster completion (cannot be lower than 200 because then commenting doesn't work)
+  undofile       = true,                    --- Sets undo to file
+  updatetime     = 100,                     --- Faster completion
+  viminfo        = "'1000",                 --- Increase the size of file history
+  wildignore     = "*node_modules/**",      --- Don't search inside Node.js modules
+  wrap           = false,                   --- Display long lines as just one line
+  writebackup    = false,                   --- Not needed
+  --- Displays correct folding markers
+  statuscolumn   = [[%s%=%l %#FoldColumn#%{foldlevel(v:lnum) > foldlevel(v:lnum - 1)? foldclosed(v:lnum) == -1? "": "": foldlevel(v:lnum) == 0? " ": " "} ]],
+}
+
+local globals = {
+  mapleader = ';',        --- Map leader key to ;
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+for k, v in pairs(globals) do
+  vim.g[k] = v
+end
+
+-- hide default statusline and ruler in dashboard
+vim.cmd([[autocmd FileType alpha set laststatus=0]])
+vim.cmd([[autocmd FileType alpha set noruler]])
