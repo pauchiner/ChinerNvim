8c30d3b095b9918cdbc8f2c2272e6bddcfe05c3d
chiner <pauchiner@icloud.com>
2023-09-13 16:05:59 +0200

    Update the ChinerNvim.lua config file
    Comments organized, more clear

--- lua/config/ChinerNvim.lua
+++ lua/config/ChinerNvim.lua
@@ -3,79 +3,79 @@ ChinerNvim = {
 
   -- Statusline
   statusline = {
-    colorscheme = 'palenight', -- To see all avaliable go to https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+    colorscheme = 'palenight', --- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
 	},
 
-  -- File manager (Tree)
+  --- File manager (Tree)
   filemanager = {
-    side = "right",      -- The side of the tree ("right" or "left")
-    indent_markers = true,  -- Indentation in the tree structure
+    side = "right",        --- The side of the tree ("right" or "left")
+    indent_markers = true, --- Identation on the tree structure
 
-    diagnostics = {      -- Diagnostic markers on the files
-      highlights = true, -- Highlight filenames with diagnostics colors
-      icons = true,
+    diagnostics = {        --- Diagnostic markers on the files
+      highlights = true,   --- Highlight filenames with diagnostics colors
+      icons = false,
     },
 
-    git = {              -- Git markers on the files
-      highlights = false, -- Highlight with colors git files
+    git = {                --- Git markers on the files
+      highlights = false,  --- Highlight with colors git files
       icons = true,
     }
   },
 
-  -- Builtin diagnostics
+  --- Builtin diagnostics
   lsp = {
-		gutter = true, -- Diagnostic symbols in the sign column
-    breadcrumb = false, -- Show the current symbols in the top bar
-    virtual_text = true, -- show virtual text (errors, warnings, info) inline messages
+		gutter = true,         --- Diagnostic symbols in the sign column
+    breadcrumb = false,    --- Show the current symbols in the top bar
+    virtual_text = true,   --- show virtual text (errors, warnings, info) inline messages
 	},
 
 	plugins = {
 
-    -- Identation like VS Code
+    --- Identation like VS Code
     identation = {
-      lines = true, -- Displays identation lines in the code (indent_blankline.nvim)
-      folding = true, -- Modern folding with markers in the gutter and mouse support (nvim-ufo)
+      lines = true,        --- Displays identation lines in the code (indent_blankline.nvim)
+      folding = true,      --- Modern folding with markers in the gutter and mouse support (nvim-ufo)
     },
 
-    -- Format code with prettier, run :Format or set formatOnSave to true.
+    --- Format code with prettier, run :Format or set formatOnSave to true.
     prettier = {
       enabled = true,
       formatOnSave = true,
     },
 
-    -- Allows to use Ctrl-jkhl to move between neovim and tmux
+    --- Allows to use Ctrl-jkhl to move between neovim and tmux
     tmux_navigation = {
       enabled = false,
     },
 
-    -- Git symbols in the UI
+    --- Git symbols in the UI
     gitsigns = {
       enabled = true,
       config = {
-        gutter = true, -- Git decorations in the gutter
-        line_blame = false -- Virtual text git blame in the current line
+        gutter = true,       --- Git decorations in the gutter
+        line_blame = false   --- Virtual text git blame in the current line
       }
     },
 
-    -- Integrate lazygit in the editor, run :Lz or :LazyGit
+    --- Integrate lazygit in the editor, run :Lz or :LazyGit
     lazygit = {
       enabled = true
     },
 
     --- EXPERIMENTAL FEATURES ---
 
-    -- Checks the spelling on the code (only english)
-    spellchecker = {
-      enabled = false
-    },
-
-    -- Allow to preview in realtime a mardown file
+    --- Allow to preview in realtime a mardown file
     markdown_preview = {
       enabled = true,
       config = {
-        mode = "dark" -- Color mode: "dark" or "light"
+        mode = "dark"       --- "dark" or "light"
       }
     },
 
+    --- [ CONTAINS BUGS ]
+    --- Checks the spelling on the code (only english)
+    spellchecker = {
+      enabled = false
+    },
 	},
 }
