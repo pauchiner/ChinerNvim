2d257470c4030188441870044cbf3f1490c0fbdc
Pau Garcia Chiner <pauchiner@MacBook-Air-de-Pau.local>
2023-11-12 23:35:51 +0100

    Update comments on the keymappings

--- lua/config/keymappings.lua
+++ lua/config/keymappings.lua
@@ -2,44 +2,44 @@ local keymap = vim.keymap.set
 local silent = { silent = true }
 local saga = { noremap = true, silent = true }
 
--- Increment/decrement
+--- Increment/decrement
 keymap('n', '+', '<C-a>', silent)
 keymap('n', '-', '<C-x>', silent)
 
--- Delete a word backwards
+--- Delete a word backwards
 keymap('n', 'dw', 'vb"_d', silent)
 
--- Select all
+--- Select all
 keymap('n', '<C-a>', 'gg<S-v>G', silent)
 
--- New tab
+--- New tab
 keymap('n', 'te', ':tabedit', silent)
 
--- Split window
+--- Split window
 keymap('n', 'ss', ':split<Return><C-w>w', silent)
 keymap('n', 'sv', ':vsplit<Return><C-w>w', silent)
 
--- Move window
+--- Move window
 keymap('n', '<Space>', '<C-w>w', silent)
 keymap('', '<C-h>', '<C-w>h', silent)
 keymap('', '<C-k>', '<C-w>k', silent)
 keymap('', '<C-j>', '<C-w>j', silent)
 keymap('', '<C-l>', '<C-w>l', silent)
 
--- Resize window
+--- Resize window
 keymap('n', '<C-w><left>', '<C-w><', silent)
 keymap('n', '<C-w><right>', '<C-w>>', silent)
 keymap('n', '<C-w><up>', '<C-w>+', silent)
 keymap('n', '<C-w><down>', '<C-w>-', silent)
 
--- Tree
+--- Tree
 keymap('n', ';l', "<cmd> NvimTreeToggle<CR>", silent)
 
--- Bufferline
+--- Bufferline
 keymap('n', '<Tab>', function () require('bufferline').cycle(1) end, silent)
 keymap('n', '<S-Tab>', function () require('bufferline').cycle(-1) end, silent)
 
--- Lsp Saga
+--- Lsp Saga
 keymap('n', 'K', function () require('lspsaga.hover'):render_hover_doc() end, silent)
 keymap("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", silent)
 keymap("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", silent)
@@ -47,7 +47,7 @@ keymap("n", "gp", "<Cmd>Lspsaga peek_definition<CR>", saga)
 keymap("n", "gd", "<Cmd>Lspsaga finder<CR>", saga)
 keymap("n", "gr", "<Cmd>Lspsaga rename<CR>", saga)
 
--- Telescope
+--- Telescope
 keymap("n", "\\\\", "<cmd>Telescope buffers prompt_prefix=<CR>", silent)
 keymap("n", "<leader>r", "<cmd>Telescope live_grep prompt_prefix=<CR>", silent)
 keymap("n", "<leader>f", "<cmd>Telescope find_files prompt_prefix=<CR>", silent)
