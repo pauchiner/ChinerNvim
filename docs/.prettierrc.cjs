09da5f599aac0a16ae8a6925b01543e7ba931475
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-01-19 12:12:03 +0100

    docs(chore): setup prettier

---
+++ docs/.prettierrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: ['prettier-plugin-astro'],
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+};
