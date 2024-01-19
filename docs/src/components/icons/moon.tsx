58b9598c7a2475acbe9af92fb3b94532aaba4c32
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-01-19 17:51:14 +0100

    docs(feat): add icons

---
+++ docs/src/components/icons/moon.tsx
@@ -0,0 +1,17 @@
+export const MoonIcon = () => (
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    stroke-width="2"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round">
+    <path
+      d="M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z"
+      fill="currentColor"
+    />
+  </svg>
+);
+
+export default MoonIcon;
---
+++ docs/src/components/icons/sun.tsx
@@ -0,0 +1,15 @@
+export const SunIcon = () => (
+  <svg
+    width="20px"
+    height="20px"
+    stroke-width="2"
+    viewBox="0 1 22 22"
+    stroke="currentColor"
+    stroke-linecap="round"
+    stroke-linejoin="round">
+    <circle cx="12" cy="12" r="4" fill="currentColor" />
+    <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
+  </svg>
+);
+
+export default SunIcon;
