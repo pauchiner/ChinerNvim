a7a637045e239b50d70d8e8c6316e0a4d330d24c
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2024-01-31 12:25:45 +0100

    feat(tmux-navigator): add auto mode

--- lua/plugins/editor/tmux.lua
+++ lua/plugins/editor/tmux.lua
@@ -1,6 +1,17 @@
+local function cond ()
+  local condition = ChinerNvim.plugins.tmux_navigation.enabled
+
+  --- Checks if nvim is running in a tmux session
+  if condition == 'auto' and os.getenv("TMUX") ~= nil then return true end
+
+  --- Manual condition
+  if condition == 'on' then return true end
+  return false;
+end
+
 return {
   "christoomey/vim-tmux-navigator",
   name = "TmuxNavigation",
-  cond = ChinerNvim.plugins.tmux_navigation.enabled,
+  cond = cond(),
   event = "BufEnter"
 }
