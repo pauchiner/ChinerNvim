18e33ec7ab4517e35ad14ea57e7251e3951487e7
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2025-02-26 18:18:03 +0100

    fix: update lualine styles

--- lua/plugins/ui/lualine.lua
+++ lua/plugins/ui/lualine.lua
@@ -21,7 +21,7 @@ return {
     },
     sections = {
       lualine_a = { "mode" },
-      lualine_b = { "branch" },
+      lualine_b = { {"branch", icon = ''} },
       lualine_c = {
         {
           "diff",
