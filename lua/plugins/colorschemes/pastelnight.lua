ad0464621b85b08fcb4b71af2fe167a8eb6d93d7
Pau Garcia Chiner <49660697+pauchiner@users.noreply.github.com>
2025-02-26 18:41:42 +0100

    fix: update pastelnight config for a better transparent compatibility

--- lua/plugins/colorschemes/pastelnight.lua
+++ lua/plugins/colorschemes/pastelnight.lua
@@ -3,6 +3,33 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    require('pastelnight').setup({
+      transparent = true,
+      styles = {
+        comments = {italic = true},
+        functions = {italic = true},
+
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+      on_highlights = function (hl, c)
+        hl.NvimTreeEndOfBuffer = {
+          bg = "NONE",
+          fg = "NONE"
+        }
+        hl.EndOfBuffer = {
+          bg = "NONE",
+          fg = "NONE"
+        }
+        hl.SignColumn = {
+          bg = c.none,
+        }
+        hl.FoldColumn = {
+          bg = c.none,
+        }
+      end
+    })
+
     vim.api.nvim_command([[colorscheme pastelnight]])
   end,
 }
